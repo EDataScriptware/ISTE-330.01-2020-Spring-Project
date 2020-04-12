3f9dbc388e3bf9e85dcf77a8a5c1982d97cfbd44
AnonymousKiddie <18145874+AnonymousKiddie@users.noreply.github.com>
2020-04-12 18:12:35 -0400

    Updated main.java
    Added Java Docs and database messages

--- src/main.java
+++ src/main.java
@@ -3,45 +3,82 @@ import javax.sql.*;
 import java.util.*;
 import java.io.*;
 
-public class main
-{
+/**
+* The main program is used to request the user to choose Register or Login 
+*
+* @author  Edward Riley, Trent Jacobson, Liam Bewley, Matthew Oelbaum, and Sayed Mobin 
+* @version 1.0
+* @since   2020-03-18 
+*/
+
+public class main {
  
    
-   public static void main(String[] args) throws DLException
-   {  
-      MySQLDatabase msd = new MySQLDatabase();
    
-      char choice;
-      try 
-      {
-         
-         msd.connect(); 
-         Papers papers = new Papers();
-         User user = new User(msd.getConnection());
+   /**
+    * 
+    * The Program is set that will request the user to 
+    * choose [R] Register or [L] Login to sign in their
+    * account.
+    * 
+    * @param are not used
+    * @throw DLException to find errors
+    * 
+    */
+    
+   public static void main(String[] args) throws DLException {  
+   
+   
+      // Instantiates this database
+      MySQLDatabase msd = new MySQLDatabase();
       
+      
+      // Set it to compare that choice is made
+      char choice;
+      
+      try {
+         
+         // To get data initialization connected
+         msd.connect(); 
+         // Connect to the papers java file 
+         Papers papers = new Papers();
+         // Connect user java file to the MySQLDatabase
+         User user = new User(msd.getConnection());
+         
+         // Display the message to see if database is able to connect (true or false), what driver is connected, and what database is connected
+         System.out.println("\n");  
+         System.out.println("MySQLDatabase Connection: " + msd.connect() + " \nDriver Loaded: " + msd.driver + "\nConnecting to the database: " + msd.dbName);
+         System.out.println("\n");  
+
+      
+         // Requests the user to pick one choice (Register or Login) 
          System.out.println("[R]egister or [L]ogin?");
          Scanner scnLoginOrRegister = new Scanner(System.in);
          choice = scnLoginOrRegister.next().charAt(0);
          System.out.println(choice);
          
-         if (choice == 'R')
-         {
+         // If R is being selected, it will be processed with next steps
+         if (choice == 'R') {
             user.register();
          }
-         else if (choice == 'L')
-         {
+         
+         // if L is being selected, it will be processed with next steps
+         else if (choice == 'L') {
             user.login();
          }
-         else 
-         {
+         
+         // If program is not working, it will display error and then exit
+         else {
             System.out.println("Error.");
             System.exit(0);
          }
-        
+         
+         // Data initialization is closed
          msd.close();
       }
-      catch (Exception e)
-      {
+      
+      // When program is not working, it will display error and then close the connection
+      catch (Exception e) {
 
          msd.connect();
          msd.close();
@@ -49,10 +86,5 @@ public class main
          throw new DLException(e, e.getMessage());
 
       }
-    
-    
-   
-   }
-   
-   
+   }  
 }
